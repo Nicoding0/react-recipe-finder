7e3e2e16c255b4eb5f1ea38436393aa97218afaf
Nicolas Martiné <nicolas57415@gmail.com>
2019-12-14 17:13:08 +0100

    fixed react router path issue

---
+++ precache-manifest.d3eb9f6cb2feed79c5ac34f9168b476f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f2965262631d9b756ae2f2e24c3c5d",
+    "url": "/react-recipe-finder/index.html"
+  },
+  {
+    "revision": "1ba32d66c5b423942056",
+    "url": "/react-recipe-finder/static/css/main.f7628ea1.chunk.css"
+  },
+  {
+    "revision": "841d7da6c9ed7bb73658",
+    "url": "/react-recipe-finder/static/js/2.56be15e5.chunk.js"
+  },
+  {
+    "revision": "1ba32d66c5b423942056",
+    "url": "/react-recipe-finder/static/js/main.78bba657.chunk.js"
+  },
+  {
+    "revision": "4895afeb88db44afefe0",
+    "url": "/react-recipe-finder/static/js/runtime-main.781931a1.js"
+  }
+]);
